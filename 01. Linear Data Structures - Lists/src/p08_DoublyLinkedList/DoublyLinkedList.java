3d3e8e9bb695459b1351160a80529d1e72745774
Taewii <p3rfec7@abv.bg>
2019-02-08 16:37:37 +0200

    DoublyLinkedList solution

--- 01. Linear Data Structures - Lists/src/p08_DoublyLinkedList/DoublyLinkedList.java
+++ 01. Linear Data Structures - Lists/src/p08_DoublyLinkedList/DoublyLinkedList.java
@@ -5,7 +5,35 @@ import java.util.function.Consumer;
 
 public class DoublyLinkedList<E> implements Iterable<E> {
 
+    private class Node {
+        private E value;
+        private Node previous;
+        private Node next;
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public Node getPrevious() {
+            return this.previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
     private int size;
+    private Node head;
+    private Node tail;
 
     public int size() {
         return this.size;
@@ -16,33 +44,115 @@ public class DoublyLinkedList<E> implements Iterable<E> {
     }
 
     public void addFirst(E element) {
-        throw new UnsupportedOperationException();
+        Node newHead = new Node(element);
+
+        if (this.size == 0) {
+            this.head = newHead;
+            this.tail = newHead;
+        } else {
+            newHead.setNext(this.head);
+            this.head.setPrevious(newHead);
+            this.head = newHead;
+        }
+
+        this.size++;
     }
 
     public void addLast(E element) {
-        throw new UnsupportedOperationException();
+        Node newTail = new Node(element);
+
+        if (this.size == 0) {
+            this.head = newTail;
+            this.tail = newTail;
+        } else {
+            newTail.setPrevious(this.tail);
+            this.tail.setNext(newTail);
+            this.tail = newTail;
+        }
+
+        this.size++;
     }
 
     public E removeFirst() {
-        throw new UnsupportedOperationException();
+        if (this.size == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Node oldHead = this.head;
+        if (this.size == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.getNext();
+            this.head.setPrevious(null);
+        }
+        this.size--;
+        return oldHead.value;
     }
 
     public E removeLast() {
-        throw new UnsupportedOperationException();
+        if (this.size == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Node oldTail = this.tail;
+        if (this.size == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = oldTail.getPrevious();
+            this.tail.setNext(null);
+        }
+        this.size--;
+        return oldTail.value;
     }
 
+    @SuppressWarnings("unchecked")
     public E[] toArray() {
-        throw new UnsupportedOperationException();
+        E[] elements = (E[]) new Object[this.size];
+
+        int counter = 0;
+        Node node = this.head;
+
+        while (node != null) {
+            elements[counter] = node.value;
+            node = node.getNext();
+            counter++;
+        }
+        return elements;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new DoublyLinkedListIterator();
+    }
+
+    private class DoublyLinkedListIterator implements Iterator {
+        Node node = head;
+
+        @Override
+        public boolean hasNext() {
+            return this.node != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new IllegalArgumentException();
+            }
+            E tmp = this.node.value;
+            this.node = this.node.next;
+            return tmp;
+        }
     }
 
     @Override
     public void forEach(Consumer<? super E> action) {
-        throw new UnsupportedOperationException();
+        Node node = this.head;
+        while (node != null) {
+            action.accept(node.value);
+            node = node.getNext();
+        }
     }
-
 }
